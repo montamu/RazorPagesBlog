53bf220cbf20c79d01e15a0fd06e02008a8b0d81
Kim Lemon <kim.lemon@yukon-sky.jp>
2024-08-23 12:50:54 +0900

    書いた人をビューに表示されるように。モデルの各要素の表示名を設定した。

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,19 +1,24 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesBlog.Models;
 public class Blog
 {
     public int Id { get; set; }
-    [StringLength(14, MinimumLength = 4)]
-    [Required]
-    public string Author { get; set; } = string.Empty;
     [StringLength(30, MinimumLength = 1)]
     [Required]
+    [DisplayName("タイトル")]
     public string Title { get; set; } = string.Empty;
-    [DataType(DataType.Date)]
-    public DateTime PostDate { get; set; }
-    [StringLength(800, MinimumLength = 10)]
+    [StringLength(14, MinimumLength = 1)]
     [Required]
+    [DisplayName("書いた人")]
+    public string Author { get; set; } = string.Empty;
+    [DataType(DataType.Date)]
+    [DisplayName("投稿日")]
+    public DateTime PostDate { get; set; }
+    [StringLength(800, MinimumLength = 1)]
+    [Required]
+    [DisplayName("内容")]
     public string Content { get; set; } = string.Empty;
     public int Like { get; set; }
 }
